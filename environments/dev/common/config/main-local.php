aee87c713519eadba48c840741d840563d45d797
happyendik <vlom4ek@gmail.com>
2022-07-07 16:26:15 +0300

    main-local.php config fix 2

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=mysql;dbname=test',
+            'dsn' => 'mysql:host=127.0.0.1;dbname=test',
             'username' => 'test',
             'password' => 'test',
             'charset' => 'utf8',
